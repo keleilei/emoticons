8ec8a49749691bf20357cda74cd49e28b7d3af40
kelei <keleilei@gmail.com>
2017-08-28 10:52:42 +0800

    integrated react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-);
+class TodoApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = { items: [], text: '' };
+    }
+
+    render() {
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'h3',
+                null,
+                'TODO'
+            ),
+            React.createElement(TodoList, { items: this.state.items }),
+            React.createElement(
+                'form',
+                { onSubmit: this.handleSubmit },
+                React.createElement('input', { onChange: this.handleChange, value: this.state.text }),
+                React.createElement(
+                    'button',
+                    null,
+                    'Add #' + (this.state.items.length + 1)
+                )
+            )
+        );
+    }
+
+    handleChange(e) {
+        this.setState({ text: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        var newItem = {
+            text: this.state.text,
+            id: Date.now()
+        };
+        this.setState(prevState => ({
+            items: prevState.items.concat(newItem),
+            text: ''
+        }));
+    }
+}
+
+class TodoList extends React.Component {
+    render() {
+        return React.createElement(
+            'ul',
+            null,
+            this.props.items.map(item => React.createElement(
+                'li',
+                { key: item.id },
+                item.text
+            ))
+        );
+    }
+}
+
+ReactDOM.render(React.createElement(TodoApp, null), document.getElementById("root"));
